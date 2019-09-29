3c7397a9075ace6bfd61e7389f70e5be15aa6121
Agafiy_Poluchlebnik <zhuravlevyuri2000@gmail.com>
2019-09-29 22:32:12 +0300

    основной скилет создан

---
+++ app/src/main/java/com/example/interactive_map/Shops.kt
@@ -0,0 +1,12 @@
+package com.example.interactive_map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Shops : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shops)
+    }
+}
